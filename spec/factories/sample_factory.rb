2114eacd4a2ab76badfc93ef05cfe1f175cffa34
Christian Ntong <finey2000@gmail.com>
2025-01-14 11:22:46 -0800

    update sample factory

--- spec/factories/sample_factory.rb
+++ spec/factories/sample_factory.rb
@@ -1,20 +1,9 @@
-DIGITS = [
-  " _ | ||_|", # 0
-  "     |  |", # 1
-  " _  _||_ ", # 2
-  " _  _| _|", # 3
-  "   |_|  |", # 4
-  " _ |_  _|", # 5
-  " _ |_ |_|", # 6
-  " _   |  |", # 7
-  " _ |_||_|", # 8
-  " _ |_| _|"  # 9
-]
+require_relative '../../lib/policy_ocr/digits'
 
 def generate_random_entry
   entry = Array.new(3) { '' }
   9.times do
-    digit = DIGITS.sample
+    digit = PolicyOcr::Digits::MAP.keys.sample
     entry[0] << digit[0, 3]
     entry[1] << digit[3, 3]
     entry[2] << digit[6, 3]
